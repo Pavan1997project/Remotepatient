f289bdcd0b544147ccc94d4708ae2c4405e8d12a
Pavan1997project <pavanpkumar097@gmail.com>
2025-09-08 13:00:10 +0530

    Update test_add_patient.py
    updated the base_dir path

--- tests/test_add_patient.py
+++ tests/test_add_patient.py
@@ -12,6 +12,7 @@ from playwright.sync_api import sync_playwright
 # ============================
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 EXCEL_FILE_PATH = os.path.join(BASE_DIR, "patient_details_updated.xlsx")
+
 BASE_URL = "https://cx-dev-client.azurewebsites.net/login"
 
 
